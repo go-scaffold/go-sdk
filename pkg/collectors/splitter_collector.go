307fdfa87c76eb6e8281366de121d9231f456005
pasdam <pasdam@users.noreply.github.com>
2023-08-04 22:25:29 +0800

    feat: add collector to split template result into multiple files

---
+++ pkg/collectors/splitter_collector.go
@@ -0,0 +1,101 @@
+package collectors
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-scaffold/go-sdk/pkg/filters"
+	"github.com/go-scaffold/go-sdk/pkg/pipeline"
+)
+
+var (
+	defaultMultiFileTemplateNamePrefix    = "mul_" // TODO: make this configurable
+	defaultMultiFileTemplateHeadersPrefix = "@@ "  // TODO: make this configurable
+)
+
+type SplitterCollector struct {
+	baseCollector
+
+	headerPrefix string
+	filter       filters.Filter
+}
+
+func NewSplitterCollector(nextCollector pipeline.Collector) *SplitterCollector {
+	filter, _ := filters.NewPatternFilter(true, fmt.Sprintf("^%s.*", defaultMultiFileTemplateNamePrefix))
+
+	return &SplitterCollector{
+		baseCollector: baseCollector{
+			next: nextCollector,
+		},
+		headerPrefix: defaultMultiFileTemplateHeadersPrefix,
+		filter:       filter,
+	}
+}
+
+func (p *SplitterCollector) Collect(args *pipeline.Template) error {
+	if !p.filter.Accept(filepath.Base(args.Path)) {
+		return p.next.Collect(args)
+	}
+
+	scanner := bufio.NewScanner(args.Reader)
+	scanner.Split(scanLines)
+	var buffer *bytes.Buffer
+	var currentTemplate *pipeline.Template
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, p.headerPrefix) { // is header, this indicates a new file
+			if currentTemplate != nil { // collect previous file
+				err := p.next.Collect(currentTemplate)
+				if err != nil {
+					return err
+				}
+			}
+			buffer = &bytes.Buffer{}
+			currentTemplate = &pipeline.Template{
+				Reader: io.NopCloser(buffer),
+				Path:   strings.ReplaceAll(strings.TrimPrefix(strings.TrimSpace(line), "@@ name="), "\"", ""), // TODO
+			}
+		} else {
+			buffer.WriteString(line)
+		}
+	}
+
+	if currentTemplate != nil {
+		err := p.next.Collect(currentTemplate)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *SplitterCollector) CreateHeaderWithName(name string) string {
+	return fmt.Sprintf("%sname=\"%s\"", defaultMultiFileTemplateHeadersPrefix, name)
+}
+
+// scanLines is a split function for a Scanner that returns each line of
+// text, stripped of any trailing end-of-line marker. The returned line may
+// be empty. The end-of-line marker is one optional carriage return followed
+// by one mandatory newline. In regular expression notation, it is `\r?\n`.
+// The last non-empty line of input will be returned even if it has no
+// newline.
+func scanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 1, data[0 : i+1], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
---
+++ pkg/collectors/splitter_collector_test.go
@@ -0,0 +1,205 @@
+package collectors
+
+import (
+	"errors"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/go-scaffold/go-sdk/pkg/pipeline"
+	"github.com/pasdam/go-utils/pkg/assertutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func Test_splitterCollector_Collect(t *testing.T) {
+	type mocks struct {
+		nextCollectResult []error
+	}
+	type data struct {
+		path    string
+		content string
+	}
+	tests := []struct {
+		name    string
+		mocks   mocks
+		args    data
+		want    []data
+		wantErr error
+	}{
+		{
+			name: "Should accept result if file starts with right prefix and extract the only file",
+			mocks: mocks{
+				nextCollectResult: []error{
+					nil,
+				},
+			},
+			args: data{
+				path:    "some-path/mul_something",
+				content: "@@ name=\"some-other-name-1\"\nsome-file-1-line-1",
+			},
+			want: []data{
+				{
+					path:    "some-other-name-1",
+					content: "some-file-1-line-1",
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Should accept result if file starts with right prefix and extract separate files",
+			mocks: mocks{
+				nextCollectResult: []error{
+					nil,
+					nil,
+				},
+			},
+			args: data{
+				path:    "some-path/mul_something",
+				content: "@@ name=\"some-other-name-1\"\nsome-file-1-line-1\nsome-file-1-line-2\n@@ name=\"some-other-name-2\"\nsome-file-2-line-1\n\n",
+			},
+			want: []data{
+				{
+					path:    "some-other-name-1",
+					content: "some-file-1-line-1\nsome-file-1-line-2\n",
+				},
+				{
+					path:    "some-other-name-2",
+					content: "some-file-2-line-1\n\n",
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Should not accept file if the name prefix is not the expected one",
+			mocks: mocks{
+				nextCollectResult: []error{
+					nil,
+				},
+			},
+			args: data{
+				path:    "some-path/something",
+				content: "@@ name=\"some-other-name-1\"\nsome-content-1\n@@ name=\"some-other-name-2\"\nsome-content-2",
+			},
+			want: []data{
+				{
+					path:    "some-path/something",
+					content: "@@ name=\"some-other-name-1\"\nsome-content-1\n@@ name=\"some-other-name-2\"\nsome-content-2",
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Should not accept file if the name prefix is not the expected one, even though the prefix is in the name",
+			mocks: mocks{
+				nextCollectResult: []error{
+					nil,
+				},
+			},
+			args: data{
+				path:    "some-path/not_mul_something",
+				content: "@@ name=\"some-other-name-1\"\nsome-content-1\n@@ name=\"some-other-name-2\"\nsome-content-2",
+			},
+			want: []data{
+				{
+					path:    "some-path/not_mul_something",
+					content: "@@ name=\"some-other-name-1\"\nsome-content-1\n@@ name=\"some-other-name-2\"\nsome-content-2",
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Should propagate error if next collector throws one at first file",
+			mocks: mocks{
+				nextCollectResult: []error{
+					errors.New("some-first-file-error"),
+				},
+			},
+			args: data{
+				path:    "some-path/mul_something",
+				content: "@@ name=\"some-other-name-1\"\nsome-content-1\n@@ name=\"some-other-name-2\"\nsome-content-2",
+			},
+			wantErr: errors.New("some-first-file-error"),
+		},
+		{
+			name: "Should propagate error if next collector throws one at second file",
+			mocks: mocks{
+				nextCollectResult: []error{
+					nil,
+					errors.New("some-second-file-error"),
+				},
+			},
+			args: data{
+				path:    "some-path/mul_something",
+				content: "@@ name=\"some-other-name-1\"\nsome-content-1\n@@ name=\"some-other-name-2\"\nsome-content-2",
+			},
+			want: []data{
+				{
+					path:    "some-other-name-1",
+					content: "some-content-1\n",
+				},
+			},
+			wantErr: errors.New("some-second-file-error"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mc := &mockCollector{}
+			for _, err := range tt.mocks.nextCollectResult {
+				mc.On("Collect", mock.Anything).Return(err).Once()
+			}
+			p := NewSplitterCollector(mc)
+
+			err := p.Collect(&pipeline.Template{
+				Path:   tt.args.path,
+				Reader: io.NopCloser(strings.NewReader(tt.args.content)),
+			})
+
+			assertutils.AssertEqualErrors(t, tt.wantErr, err)
+			calls := mc.Calls
+			assert.Equal(t, len(tt.mocks.nextCollectResult), len(calls))
+			for i := 0; i < len(tt.want); i++ {
+				got := calls[i].Arguments.Get(0).(*pipeline.Template)
+				assert.Equal(t, tt.want[i].path, got.Path)
+				gotContent, err := io.ReadAll(got.Reader)
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want[i].content, string(gotContent))
+			}
+		})
+	}
+}
+
+func Test_splitterCollector_CreateHeaderWithName(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Should return correct header",
+			args: args{
+				name: "some-name",
+			},
+			want: "@@ name=\"some-name\"",
+		},
+		{
+			name: "Should return correct header with a different name",
+			args: args{
+				name: "some-other-name",
+			},
+			want: "@@ name=\"some-other-name\"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := NewSplitterCollector(nil)
+
+			got := p.CreateHeaderWithName(tt.args.name)
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
